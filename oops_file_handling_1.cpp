56627469427bcdc8586cd9de5c8fd2a3e611fc48
Aditya Jain <adityajainghetal@gmail.com>
2024-09-11 14:55:44 +0530

    complete cpp

---
+++ oops_file_handling_1.cpp
@@ -0,0 +1,52 @@
+//file hinding
+
+
+//						ios
+			
+//			istream                 ostream (standard)
+						
+						
+//						iostream
+					
+//-----------------------------------------------------			
+						
+			
+//			ifstream                 ofstream
+		
+			
+//						fstream
+
+//----------------------------------------------
+
+
+//define in header file (fstream)
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+	
+	
+	
+
+	
+
+int main(){
+	ofstream Out("C:\\Users\\Ghetal\\Desktop\\C++\\abcd.txt");
+	
+	Out.close();	
+
+}
+
+
+
+
+//input opertion : file read
+// class used: ifstream
+
+ 
+ 
+// output opertion : file write
+// class used:ofstream
+
+
+//intput/output --> read/write ---> class fstream
